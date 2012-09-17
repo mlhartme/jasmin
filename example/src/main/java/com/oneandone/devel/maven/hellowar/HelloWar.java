4f2a3b9c2e28971380fc511f1cef5995553a1f56
Michael Hartmeier <mlhartme@github.com>
2012-09-17 15:08:56 +0200

    sample application

---
+++ example/src/main/java/com/oneandone/devel/maven/hellowar/HelloWar.java
@@ -0,0 +1,19 @@
+package com.oneandone.devel.maven.hellowar;
+
+import javax.servlet.http.HttpServlet;
+import java.io.Writer;
+
+public class HelloWar extends HttpServlet {
+    protected void doGet(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp)
+            throws javax.servlet.ServletException, java.io.IOException {
+        Writer writer;
+
+        resp.setContentType("text/html");
+        writer = resp.getWriter();
+        writer.write("<html><body><h1>Hello, Jasmin</h1>\n");
+        writer.write("<a href='xml/jasmin/admin/'>Admin page</a>");
+        writer.write("</body></html>\n");
+        writer.close();
+    }
+
+}
