0cd45dc933967a5e76fcad532a6c7482dc4505e0
Michael Hartmeier <mlhartme@github.com>
2012-09-17 15:55:18 +0200

    ass css sample

--- example/src/main/java/example/hello/Hello.java
+++ example/src/main/java/example/hello/Hello.java
@@ -11,11 +11,11 @@ public class Hello extends HttpServlet {
         resp.setContentType("text/html");
         writer = resp.getWriter();
         writer.write("<html><body><h1>Hello, Jasmin</h1>\n");
-        writer.write("<ul>\n");
-        writer.write("  <a href='xml/jasmin/admin/'>Admin page</a>\n");
-        writer.write("  <a href='xml/jasmin/get/no-expires/hello-jasmin-foo-bar/js/lead'>foo/bar.js</a>\n");
-        writer.write("  <a href='xml/jasmin/get/no-expires/hello-jasmin-foo-bar/js-min/lead'>foo/bar.js minimiert</a>\n");
-        writer.write("</ul>\n");
+        writer.write("  <a href='xml/jasmin/admin/'>Admin page</a><br/>\n");
+        writer.write("  <a href='xml/jasmin/get/no-expires/hello-jasmin-foo-bar/js/lead'>foo/bar.js</a><br/>\n");
+        writer.write("  <a href='xml/jasmin/get/no-expires/hello-jasmin-foo-bar/js-min/lead'>foo/bar.js minimiert</a><br/>\n");
+        writer.write("  <a href='xml/jasmin/get/no-expires/hello-jasmin-foo-bar/css/lead'>foo/bar.css</a><br/>\n");
+        writer.write("  <a href='xml/jasmin/get/no-expires/hello-jasmin-foo-bar/css-min/lead'>foo/bar.css minimiert</a><br/>\n");
         writer.write("</body></html>\n");
         writer.close();
     }
